2eb5ffb70c1afeb9f48b920aa4a6a9489e34629d
Louis <louis@vulpem.com>
2022-10-07 17:31:16 +0200

    add buggish mnemonic test case

--- tests/test_transaction_service.py
+++ tests/test_transaction_service.py
@@ -131,3 +131,44 @@ async def test_send_fee_nonlast_amp_confidential_pset():
     wally.psbt_finalize(pset)
     tx = wally.psbt_extract(pset)
     assert tx is not None
+
+@pytest.mark.asyncio
+async def buggish_mnemonic():
+    mnemonic = "begin trouble invite bus reform wave crouch floor bring detect record bag"
+    session = make_session('testnet-liquid')
+    walletSvc = WalletService(session, FilePinDataRepository('pin_data.json'))
+    walletSvc.create_wallet(mnemonic, TEST_PASSWORD)
+    # walletSvc.login_with_pin(TEST_PASSWORD)
+    lockerSvc = await Locker.create()
+    accountSvc = AccountService(session, lockerSvc)
+    transactionSvc = TransactionService(session, lockerSvc)
+    # gdkAPI = GdkAPI(session)
+    # accountNameMultisig = 'Securities'
+    utxo0 = accountSvc.get_utxo('1f78b56be4ecb6591b4c6069e629f39470c7569d3737e7d8223cf76a77dc3015', 0)
+    utxo1 = accountSvc.get_utxo('82436b9e07f0f9d909d73faade015bdcd4374961cb0e80b9a23f76b8ae444fa3', 0)
+
+
+    FEE = 500
+    receiveAddr = 'vjU5inAPDKufZSfr7b2y7JAmrNSdjZA2WdbAY8DySrgoQ8cKA4R6D4hDR5crxdamchiakXMoENAYyDss' 
+
+    outputs = [
+        {'address': receiveAddr, 'amount': 1, 'asset': 'bea126b86ac7f7b6fc4709d1bb1a8482514a68d35633a5580d50b18504d5c322', 'blinder_index': 0},
+        {'address': None, 'amount': FEE, 'asset': '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49', 'blinder_index': None},
+        {'address': 'vjTvPHdcJFZrYL9LmFpPootd1tmWqzugF9MXwhet6cdeCKEK6WJrb2mPEQGw7WNpikAoTq9ui22GU2pS', 'amount': 100000000-FEE, 'asset': '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49', 'blinder_index': 0}
+    ]
+    
+    b64 = transactionSvc.create_pset(
+        [u.to_pset_input_args() for u in [utxo0, utxo1]], 
+        outputs
+    )
+    
+    blinded = transactionSvc.blind_pset(b64)
+    print(blinded)
+    signed = transactionSvc.sign_pset(blinded)    
+    assert signed is not None
+
+    pset = wally.psbt_from_base64(signed)
+    wally.psbt_finalize(pset)
+    tx = wally.psbt_extract(pset)
+    print(tx)
+    # assert tx is not None
